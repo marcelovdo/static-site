23f309ac6b607e81f226f8477a2543724f7c3132
marcelovdo <vdomarcelo@gmail.com>
2024-05-31 18:38:40 -0300

    getting block types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,25 @@
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
-    return list(filter(lambda block: block != "", markdown.split("\n\n")))
+    return list(map(lambda block: block.strip("\n "), filter(lambda block: block != "", markdown.split("\n\n"))))
+
+def block_to_block_type(block):
+    for i in range(6):
+        if len(block) > i+2 and ("#"*(i+1)) + " " == block[:i+2]:
+            return block_type_heading
+    if block[:3] == "```" and block[-3:] == "```":
+        return block_type_code
+    lines = block.split("\n")
+    if all([line[0] == ">" for line in lines]):
+        return block_type_quote
+    if all([line[:2] == "* " or line[:2] == "- " for line in lines]):
+        return block_type_unordered_list
+    if all([lines[i][:2+len(str(i+1))] == f"{i+1}. " for i in range(len(lines))]):
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -12,13 +12,79 @@ This is another paragraph with *italic* text and `code` here
 This is the same paragraph on a new line
 
 * This is a list
-* with items"""
+* with items
+        """
         blocks = markdown_to_blocks(text)
         self.assertListEqual(blocks, [
-            "\nThis is **bolded** paragraph",
+            "This is **bolded** paragraph",
             "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
             "* This is a list\n* with items"
         ])
+
+    def test_get_type_heading(self):
+        block = "# Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_heading)
+
+        block = "#### Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_heading)
+
+        block = "###### Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_heading)
+
+        block = "####### Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_get_type_code(self):
+        block = "```Some code\nother code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_code)
+
+        block = "```Some code\nother code"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_get_type_quote(self):
+        block = ">Some quote\n>other quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_quote)
+
+        block = ">Some quote\nother quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_get_type_unordered_list(self):
+        block = "* Some item\n* other item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_unordered_list)
+
+        block = "- Some item\n- other item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_unordered_list)
+
+        block = "* Some item\nother item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_get_type_ordered_list(self):
+        block = "1. Some item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+        block = "1. Some item\n2. other item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+        block = "1. a\n2. b\n3. c\n4. d\n5. e\n6. f\n7. g\n8. h\n9. i\n10. j"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+        block = "1. Some item\nother item\n3. last item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, block_type_paragraph)
 
 if __name__ == "__main__":
     unittest.main()
